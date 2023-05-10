639fd5479240553f7277aa4a827799931495bf09
Daniel Hernandez <daniher02@gmail.com>
2023-05-10 18:55:07 -0400

    se hace CURD de las task min 2:12:49

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -24,6 +24,10 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("task/", views.task, name="task"),
     path("task/create/", views.create_task, name="create_task"),
+    path("task/completed/", views.task_completed, name="completed_task"),
+    path("task/<int:task_id>/", views.task_detail, name="detail_task"),
+    path("task/<int:task_id>/complete/", views.complete_task, name="complete_task"),
+    path("task/<int:task_id>/delete/", views.delete_task, name="delete_task"),
     path("logout/", views.signout, name="logout"),
     path("signin/", views.signin, name="signin"),
 ]
--- task/views.py
+++ task/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
+from .models import Task
+from django.utils import timezone
 
 # Create your views here.
 
@@ -57,9 +59,10 @@ def signup(request):
 
 
 def task(request):
-    return render(request, "task.html")
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, "task.html", {'tasks': tasks})
 
-
+# ? INSERT
 def create_task(request):
     if request.method == "GET":
         return render(request, "create_task.html", {"form": TaskForm})
@@ -79,11 +82,44 @@ def create_task(request):
                 {"form": TaskForm, "error": "Datos invalidos"},
             )
 
+# ? READ & UPDATE
+def task_detail(request, task_id):
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {'task': task, 'form':form})
+    else:
+        try:
+            # ? busca en el objeto Task donde
+            # ? el id sea el que se le paso y el usuario sea el mismo que hace la solicitud
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('task')
+        except ValueError:
+            return render(request, 'task_detail.html', {'task': task, 'form':form, 'error': 'Error al actualizar los datos'})
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('task')
+
+# ? DELETE
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task')    
+
+def task_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, "task_completed.html", {'tasks': tasks})    
 
 def signout(request):
     logout(request)
     return redirect("home")
-
 
 # ? inicio de sesion con AuthenticationForm
 def signin(request):
